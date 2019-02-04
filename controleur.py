272175b2528f607876033c6587d7910fc862e3e3
bernaelo <eloi.bernard@iut2.univ-grenoble-alpes.fr>
2019-02-04 22:04:43 +0100

    oui

--- controleur.py
+++ controleur.py
@@ -1,9 +1,24 @@
 import pygame
-
+import terrain as t
+import vueJouer as vj
 from pygame.locals import *
-
-
 pygame.init()
+
+terrain=t.Terrain()
+vue = vj.Vue()
+terre = pygame.image.load('terre.png')
+vue.Update(terrain)
+
+run=True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    pygame.display.flip()
+
 
 
 
