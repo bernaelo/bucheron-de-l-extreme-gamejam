5421b0367718aec1de6809655242e31f9387754a
bernaelo <45598228+bernaelo@users.noreply.github.com>
2019-02-04 19:02:18 +0100

    Add files via upload

--- vueJouer.py
+++ vueJouer.py
@@ -1,11 +1,18 @@
 import pygame
-import terrain as t
+import typecase.typecase as tc
 
-class Terrain(object):
+class Vue(object):
 
     def __init__(self):
         self.fenetre = pygame.display.set_mode((1600, 800))
 
-    def getWind(self):
+    def getFenetre(self):
         return self.fenetre
+
+    def Update(self,terrain):
+        terre = pygame.image.load('terre.png')
+
+        for i in range(0,len(terrain.getCases())-1):
+            if terrain.getCases()[i].getType()==tc.TERRE:
+                self.fenetre.blit(terre,)
 
