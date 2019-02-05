0e56ee083a467bf3df536b3486b833c91d171fd2
bernaelo <eloi.bernard@iut2.univ-grenoble-alpes.fr>
2019-02-05 21:21:06 +0100

    1

--- vueJouer.py
+++ vueJouer.py
@@ -11,34 +11,40 @@ class Vue(object):
         self.immobileGauche = [pygame.image.load('Bucheron-Stop-Left0.png'), pygame.image.load('Bucheron-Stop-Left1.png')]
         self.attaqueDroite = [pygame.image.load('att D0.png'), pygame.image.load('att D1.png'), pygame.image.load('att D2.png'), pygame.image.load('att D3.png'), pygame.image.load('att D4.png')]
         self.attaqueGauche = [pygame.image.load('att G0.png'), pygame.image.load('att G1.png'), pygame.image.load('att G2.png'), pygame.image.load('att G3.png'), pygame.image.load('att G4.png')]
+        self.nuagefD = [pygame.image.load('NUAGE FIN D.png'), pygame.image.load('NUAGE FIN D2.png')]
+        self.nuagefG = [pygame.image.load('NUAGE FIN G.png'), pygame.image.load('NUAGE FIN G2.png')]
+        self.nuage = [pygame.image.load('nuage.png'), pygame.image.load('NUAGE mouv2.png')]
+        self.terre = pygame.image.load('terre.png')
+        self.herbe = pygame.image.load('herbe.png')
         self.stopCount =0
         self.walkCount = 0
+        self.nuageCount=0
 
 
     def Update(self,terrain,bu,fenetre):
-        terre = pygame.image.load('terre.png')
-        herbe = pygame.image.load('herbe.png')
-        self.nuagefD = [pygame.image.load('NUAGE FIN D.png'),pygame.image.load('NUAGE FIN D2.png')]
-        self.nuagefG = [pygame.image.load('NUAGE FIN G.png'),pygame.image.load('NUAGE FIN G2.png')]
-        self.nuage = [pygame.image.load('nuage.png'), pygame.image.load('NUAGE mouv2.png')]
+
+
         fenetre.blit(pygame.image.load('background.png'),(0,0))
 
         for i in range(0,len(terrain.getCases())-1):
             for j in range(0,len(terrain.getCases()[i])-1):
                 if terrain.getCases()[i][j].getType()==tc.typecase.TERRE:
-                    fenetre.blit(terre,(j*50,i*50))
+                    fenetre.blit(self.terre,(j*50,i*50))
                 elif terrain.getCases()[i][j].getType()==tc.typecase.HERBE:
-                    fenetre.blit(herbe,(j*50,i*50))
+                    fenetre.blit(self.herbe,(j*50,i*50))
                 elif terrain.getCases()[i][j].getType()==tc.typecase.NUAGE:
                     #fenetre.blit(nuage,(j*50,i*50))
-                    fenetre.blit(self.nuage[self.stopCount // 4], (j*50, i*50))
+                    fenetre.blit(self.nuage[self.nuageCount // 10], (j*50, i*50))
                 elif terrain.getCases()[i][j].getType() == tc.typecase.NUAGED:
                     #fenetre.blit(nuagefD, (j * 50, i * 50))
-                    fenetre.blit(self.nuagefD[self.stopCount // 4], (j * 50, i * 50))
+                    fenetre.blit(self.nuagefD[self.nuageCount // 10], (j * 50, i * 50))
                 elif terrain.getCases()[i][j].getType() == tc.typecase.NUAGEG:
                     #fenetre.blit(nuagefG,(j*50,i*50))
-                    fenetre.blit(self.nuagefG[self.stopCount // 4], (j * 50, i * 50))
+                    fenetre.blit(self.nuagefG[self.nuageCount // 10], (j * 50, i * 50))
 
+        self.nuageCount +=1
+        if self.nuageCount>19:
+            self.nuageCount=0
 
         if self.walkCount >= 12:
             self.walkCount=0
